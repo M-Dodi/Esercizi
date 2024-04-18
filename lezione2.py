3d001c448192fb9ccc99b264b5a7e50910a7f727
M-Dodi <marindodi@libero.it>
2024-04-18 12:34:26 +0200

    Primo esercizio2 risolto

--- lezione2.py
+++ lezione2.py
@@ -1,7 +1,13 @@
 # Marin Dodi
-#Personal massage
+#Personal massage: use a variable to rapresent a persons name
+# then write a massage to that person
 
 print("Hello World")
+
+#This variable contains the name
 name: str = "Mario"
+
+#This variable contains the massage
 massage: str = f"Ciao {name}, ti va di imparare un pò di python insieme?"
+
 print(massage)
