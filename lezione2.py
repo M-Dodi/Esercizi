7c04509a1cdf696fa37da91d1ab22662e7f5f659
M-Dodi <marindodi@libero.it>
2024-04-18 12:51:05 +0200

    Esercizio2

--- lezione2.py
+++ lezione2.py
@@ -11,3 +11,18 @@ name: str = "Mario"
 massage: str = f"Ciao {name}, ti va di imparare un pò di python insieme?"
 
 print(massage)
+
+
+
+#Name case
+#Questa è la variabile di una persona
+name: str = "Mario"
+
+
+#Altro modo
+#name_lower: str = name.lower()
+#name_upper: str = name.upper()
+#print(f"{name}, {name_upper}, {name lower}")
+
+
+print(f"{name}, {name,upper()}, {name,lower()}")
