abd732b8e0775d0df961d9020c7ea23ea61fd935
M-Dodi <marindodi@libero.it>
2024-04-19 12:11:19 +0200

    Lezione4 prova

--- lezione4.py
+++ lezione4.py
@@ -1,1 +1,9 @@
-def substract(x:float,y:float) -> float
+def substract(x:float,y:float) -> float:
+    
+    print(f"x={x}, y={y}")
+    return x - y
+
+x = 3
+y = 5
+out = substract(x,y)
+print(f'La diff è {out}')
