e1ab541d53e6d6ddc640dc87c8c3ff1917440609
M-Dodi <marindodi@libero.it>
2024-05-07 19:22:58 +0200

    Esercizi in classe

--- lezione5/lezione5bis.py
+++ lezione5/lezione5bis.py
@@ -21,3 +21,106 @@ def is_palindrome(x):
 
 x = 12345
 print(f"L'intero{x} è palindromo? {is_palindrome(x)}")
+
+
+
+class Person:
+   
+   def __init__(self,name,age):
+      self.name = name
+      self.age = age
+
+alice = Person("Alice", 45)
+bob = Person("Bob", 36)
+
+print(f" nome = {bob.name}")
+
+if alice.age > bob.age:
+   print(f"The odest is:{alice.name}")
+elif bob.age > alice.age:
+   print(f"The oldest is:{bob.name}")
+else:
+   print("They have the same age")
+    
+
+people = ["Alice", 45, "Bob", 36 "Marin", 40, "Martin", 38, "Andi", 32]
+   
+min_age = 
+
+
+
+ #1- Write a class called Student with the attributes name(str) and study program(str) 
+ #2- Create three instances, one for yourself, one for your left and right neighbour.
+ #3- Add a method print info that prints the name and studyProgram of a Student. test
+ #your method on the object
+ #4- Modyfe the code  and add age and gender to atributes, modify the printing methods too
+
+class Student:
+   
+   
+
+   def __init__(self,name: str,studyProgram: str):
+      self.name: str = name
+      self.studyProgram: str = studyProgram
+      self.age: int = None
+      self.gender: str = None
+
+
+   def __str__(self) -> str:
+       return f'Person(name={self.name},studyProgram={self.studyProgram})'
+
+   def set_age(self, new_age: int):
+       self.age = new_age
+
+   def set_gender(self, new_gender: str):
+       self.gender = new_gender
+
+
+me = Student("Marin", "History")
+left = Student("Alessandro", "Informatics")
+right = Student("Markian", "Biology")
+   
+print(left)   
+
+left.set_age(22)
+left.set_gender("M")
+
+print(left)
+"""
+Exersise 3
+Given a class Animal for each task test our changes 1) create two realistic instance of
+Animals 2) print the name of each object 3) Change the amount of legs of one object
+usin the dot notation 4) add a method setLegs() to set the legs of an object end repeat usin the 
+method 5) Add a method getLegs( to return the amount of legs)
+6) add a method amed printInfo() that print all atrributes of the Animal
+
+"""
+
+class Animal:
+
+   def __init__(self,name: str, legs: int =0):
+      self.name: str = name
+      self.legs: int = self.get_legs(legs)
+
+
+   def  set_legs(self, new_legs: int):
+        if new_legs >= 0:
+           self.legs = new_legs
+        else:
+           self.legs = 0  
+      
+   
+   def get_legs(self) -> int:
+        return self.legs
+
+
+
+   def __str__(self) -> str:
+        return f'{self.name.capitalize()}(legs{self.get_legs})
+
+        
+cane = Animal("cane", 4) 
+
+print(cane)
+
+
--- lezione5/lezione5bis2.py
+++ lezione5/lezione5bis2.py
@@ -34,6 +34,7 @@ print(f"L'elemento più usato è: {majority_element(input_nums)}")
 
 
 
+
 #Dato una lista di numeri spostare gli zeri alla fine della lista mantenendo l'ordine dei numeri originali
 
 def move_zeroes(nums: list[int]):
@@ -47,3 +48,43 @@ input_nums = [0, 1, 0, 3, 12]
 
 print(f"La nuova lista è: {move_zeroes(input_nums)}")
           
+
+
+
+
+def visit_tree(tree: dict[int, list[int]], n: int):
+    print(n)
+
+    left_child, right_child = tree[n]   
+
+    if left_child != None:
+        visit_tree(tree, left_child)
+    if right_child != None:
+        visit_tree(tree, right_child)   
+
+tree ={1:[2,3], 2:[4,5], 3:[None,None], 4:[None,None], 5:[None,None]} 
+
+visit_tree(tree, 1)
+
+
+def visiting_tree_iterative(tree: dict[int, list[int]], root: int):
+    
+    stack: list[int] = [root]
+
+    while stack: # while len(stack) != 0
+        curr_node = stack.pop(0)
+        
+        
+        if curr_node:
+            print(curr_node)
+            
+            left_child, right_child = tree[curr_node]
+            
+            if left_child:
+                stack.append(left_child)
+            if right_child:
+                stack.append(right_child)
+
+tree ={1:[2,3], 2:[4,5], 3:[None,None], 4:[None,None], 5:[None,None]} 
+
+visiting_tree_iterative(tree, 1)
