be09683fb66af3c5362265d26a2fd13c8e4f2555
Nio4124 <antoniozottola41204@gmail.com>
2024-05-31 14:03:27 +0200

    new

---
+++ prova.py
@@ -0,0 +1,14 @@
+i: int = 0
+test_value: int = 5
+
+assert i == 0, f"Error, the variable must be {test_value} insted is{i}"
+
+
+
+def sum(a: int, b: int):
+    return 0
+result = sum(a=5, b=2)
+test_value: int = 7
+
+assert result == test_value, f"errore the variable must be {test_value} instead is {result}"
+    
---
+++ testing.py
@@ -0,0 +1,15 @@
+import unittest
+from . .Esercizi1 import Calc
+
+
+
+
+
+class Calc:
+    def __init__(self, a, b) -> None:    
+        self.a = a
+        self.b = b
+
+    def get_sum(self):
+
+        return self.a + self.b    
